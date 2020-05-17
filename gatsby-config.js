eebec71abbba76586cf114a86ef5f42ee6dcf4b7
Anjali G <anjalyyy1@gmail.com>
2020-05-17 19:34:33 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Shoe Shop`,
+    description: `The best shoe shop in the town...`,
+    author: `Anjali Gupta`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
