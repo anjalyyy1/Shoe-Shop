adf4ddcff68d912bb8b72e49048462e480489539
Anjali G <anjalyyy1@gmail.com>
2020-06-05 23:07:37 +0530

    navbar completed

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Link } from 'gatsby';
+import logo from '../../images/logo.png';
+import { map } from 'lodash';
+import { FaOpencart } from 'react-icons/fa';
+import './navbar.css';
+
+const links = [
+  {
+    path: `/`,
+    text: 'Home'
+  },
+  {
+    path: `/about`,
+    text: 'About'
+  }
+];
+
+const NavBar = props => {
+  const [navBarOpen, setNavBar] = useState(false);
+
+  return (
+    <nav
+      className='navbar-test navbar navbar-expand-sm navbar-light'
+      // style={{ backgroundColor: '#fce228' }}
+    >
+      <Link to='/' className='navbar-brand'>
+        <div style={{ width: '50px' }}>
+          <img src={logo} alt='Shoe Shop' className='img-fluid' />
+        </div>
+      </Link>
+      <button
+        className='navbar-toggler'
+        type='button'
+        onClick={() => setNavBar(!navBarOpen)}
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div
+        className={`collapse navbar-collapse justify-content-end ${
+          !navBarOpen && 'show'
+        }`}
+      >
+        <ul className='navbar-nav '>
+          {map(links, (eachLink, index) => {
+            return (
+              <li key={index} className='nav-item'>
+                <Link to={eachLink.path} className='nav-link'>
+                  {eachLink.text}
+                </Link>
+              </li>
+            );
+          })}
+
+          <li className='nav-item ml-sm-3 .cart-icon'>
+            <FaOpencart />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
